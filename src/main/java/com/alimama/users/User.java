65566c859f1b5926493534a5aeca60e0366b5217
eyasuyid <eyasuyid@gmail.com>
2023-01-23 22:16:01 +0300

    employee classes created

--- src/main/java/com/alimama/users/User.java
+++ src/main/java/com/alimama/users/User.java
@@ -4,13 +4,17 @@ public interface User {
 	abstract int getId();
 	abstract void setId(int id);
 	abstract String getFirstName();
-	abstract void setFirstName(String fistName);
+	abstract void setFirstName(String firstName);
 	abstract String getLastName();
 	abstract void setLastName(String lastName);
 	abstract String getPhone();
 	abstract void setPhone(String phone);
 	abstract String getEmail();
 	abstract void setEmail(String email);
-	public String getHashedPassword();
-	public void setHashedPassword(String hashedPassword);
+	abstract String getHashedPassword();
+	abstract void setHashedPassword(String hashedPassword);
+	abstract String getShift();
+	abstract void setShift(String shift);
+	abstract String getStatus();
+	abstract void setStatus(String status);
 }
--- src/main/java/com/alimama/users/Waiter.java
+++ src/main/java/com/alimama/users/Waiter.java
@@ -1,87 +1,77 @@
 package com.alimama.users;
 
-public class Customer implements User{
-	
-	private String firstName, lastName, email, phone, hashedPassword;
+public class Waiter {
+	private String firstName, lastName, email, phone, hashedPassword, shift;
 	private int id;
-
-	public Customer(String fistName, String lastName, String email, String phone, String hashedPassword) {
-		this.firstName = fistName;
+	
+	public Waiter(String firstName, String lastName, String email, String phone, String hashedPassword, String shift,
+			int id) {
+		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.phone = phone;
 		this.hashedPassword = hashedPassword;
-	}
-
-	public Customer(int id, String fistName, String lastName, String email, String phone,  String hashedPassword) {
+		this.shift = shift;
 		this.id = id;
-		this.firstName = fistName;
-		this.lastName = lastName;
-		this.email = email;
-		this.phone = phone;
-		this.hashedPassword = hashedPassword;
-	}
-
-	public Customer() {
-
 	}
 	
-	public int getId() {
-		return id;
+	public Waiter(String firstName, String lastName, String email, String phone, String hashedPassword, String shift) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+		this.hashedPassword = hashedPassword;
+		this.shift = shift;
 	}
-
-	public void setId(int id) {
-		this.id = id;
+	
+	public Waiter() {
 	}
 
 	public String getFirstName() {
 		return firstName;
 	}
-
-	public void setFirstName(String fistName) {
-		this.firstName = fistName;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-
 	public String getLastName() {
 		return lastName;
 	}
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 	public String getHashedPassword() {
 		return hashedPassword;
 	}
-
 	public void setHashedPassword(String hashedPassword) {
 		this.hashedPassword = hashedPassword;
 	}
-	
 	public String getShift() {
-		return null;
+		return shift;
 	}
 	public void setShift(String shift) {
+		this.shift = shift;
 	}
 	public String getStatus() {
 		return null;
 	}
 	public void setStatus(String status) {
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 }
